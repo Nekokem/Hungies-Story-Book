d346cb28b8113a73bbf8b4f154c38e1c12579f5e
Katie <kamytherabbit@gmail.com>
2019-09-23 15:45:53 -0600

    Added a title page

--- Hungies-Story-book/Assets/Scripts/ActionControler.cs
+++ Hungies-Story-book/Assets/Scripts/ActionControler.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.Events;
 
 public class ActionControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed = 0.5f, scaleSpeed = 10f, rotationSpeed = 10f;
+    private Vector3 location, scales, rotationLocation;
+    private UnityAction transformAction;
+
+    public void IsMove()
     {
-        
+        location.y = speed * Time.deltaTime;
+        transform.Translate(location);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IsScale()
     {
-        
+        scales.Set(scaleSpeed, scaleSpeed,scaleSpeed);
+        transform.localScale += scales * Time.deltaTime;
     }
+
+    public void IsRotation()
+    {
+        rotationLocation.x = rotationSpeed * Time.deltaTime;
+    }
+    
 }
