8860f58b67f39b889bb84871779987af65db3264
Katie <kamytherabbit@gmail.com>
2019-09-23 15:44:58 -0600

    Added Game Action Handler Script

---
+++ Hungies-Story-book/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public UnityEvent hanlderEvent;
+    public GameAction gameActionObj;
+
+    private void Start()
+    {
+        gameActionObj.action += Action;
+    }
+
+    private void Action()
+    {
+        hanlderEvent.Invoke();
+    }
+}
