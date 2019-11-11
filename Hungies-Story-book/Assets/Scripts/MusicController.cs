4df2828e199d051168454dcdbb640726ae7ddd54
Katie Morley <kamytherabbit@gmail.com>
2019-11-11 13:33:12 -0700

    fixed the music script added tissues

--- Hungies-Story-book/Assets/Scripts/MusicController.cs
+++ Hungies-Story-book/Assets/Scripts/MusicController.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class MusicController : MonoBehaviour
 {
-    private AudioSource _audioSource;
+    
 
     private void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
-        _audioSource = GetComponent<AudioSource>();
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+
     }
     
-    public void PlayMusic()
-    {
-        if (_audioSource.isPlaying) return;
-        _audioSource.Play();
-    }
-
-    public void StopMusic()
-    {
-        _audioSource.Stop();
-    }
+   
 }
