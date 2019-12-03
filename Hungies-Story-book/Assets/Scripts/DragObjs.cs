27fc5f843305c8e12080a9a050fb1ff38a62015b
Katie Morley <kamytherabbit@gmail.com>
2019-12-02 18:18:46 -0700

    added dragObjs script

---
+++ Hungies-Story-book/Assets/Scripts/DragObjs.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjs : MonoBehaviour
+{
+    public bool draggy = false;
+    public bool collisionOn = false;
+    private Vector3 position;
+    
+   
+    public void Start()
+    {
+        position = gameObject.transform.position;
+        draggy = true;
+    }
+
+    public void drag()
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void drop()
+    {
+        if (!collisionOn)
+        {
+            gameObject.transform.position = position;
+        }
+
+        draggy = false;
+    }
+}
